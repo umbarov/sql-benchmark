8d5e71b4b8206dc9a9eb4f4f5a293e989e4a8952
Ulugbek Umbarov <u.umbarov@gmail.com>
2020-03-27 12:30:50 +0100

    First commit

---
+++ SqlBenchmark/MsSqlConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Microsoft.Extensions.Configuration;
+
+namespace SqlBenchmark
+{
+	[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+	[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+	public class MsSqlConnection
+	{
+		private string _connectionString = "";
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			var config = new ConfigurationBuilder()
+				.AddJsonFile("./appsettings.json")
+				.Build();
+
+			_connectionString = config["MsSqlConnectionStrings:DefaultConnection"];
+		}
+
+		[Benchmark]
+		public void Connect()
+		{
+			using (var con = new SqlConnection(_connectionString))
+			{
+				con.Open();
+			}
+		}
+	}
+}
